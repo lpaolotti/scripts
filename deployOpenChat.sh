d7b550b248e819aae32cea36a758744b8851e06a
Lucas Paolotti <lpaolotti@gmail.com>
2015-03-19 18:41:34 +0100

    Ajout d'un script de déploiement pour le projet OpenChat

---
+++ deployOpenChat.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+rm -r openChat
+git clone https://github.com/AntSoft/openChat.git
+cd openChat
+npm update
+
+echo -n "Entrez le port : "
+read PORT
+
+#Remplacement du port
+sed -e '/"port"/ s/[0-9][0-9]*/'$PORT'/' config/developpement.json > tmp
+cat tmp > config/developpement.json
+rm tmp
